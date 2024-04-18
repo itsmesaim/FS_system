0c77468b469b49fb9dbeb41a78f2222b8a629f0f
itsmesaim <saimkaskar69@gmail.com>
2024-04-18 21:58:24 +0530

    first version

---
+++ src/db/conn.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const mongoose = require('mongoose');
+
+
+    mongoose.set("strictQuery", false);
+
+    mongoose.connect("mongodb+srv://saimkaskar1:l7TXntK1l6h8vKct@cluster0.nhzz0a4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        .then(() => {
+            console.log(`Successfully Connected to Mongodb`);
+        }).catch((e) => {
+            console.log("Unable to Connect to Mongodb ", e);
+        })
+
